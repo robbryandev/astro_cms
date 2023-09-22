aed26da1dd8ab0f6fb90b52ce26798ef5ae3b49d
robbryandev <robbryandev@gmail.com>
2023-09-22 15:51:44 -0700

    Support nested routes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # astro wordpress test
 
 uses the wordpress plugin [blank slate](https://wordpress.org/themes/blankslate/) to maintain a consistent style between the wordpress editor and astro output
+
+nested routes supported through [Nested Pages](https://wordpress.org/plugins/wp-nested-pages/)
--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,7 +1,20 @@
+import type { WpPage } from "@/types/Page"
+
+const wordpress = import.meta.env.WORDPRESS_URL;
+
 export function getPageEndpoint(id: number) {
-    return `${import.meta.env.WORDPRESS_URL}/wp-json/wp/v2/pages/${id}`
+    return `${wordpress}/wp-json/wp/v2/pages/${id}`
 }
 
-export function getPageEndpointBySlug(slug: string) {
-    return `${import.meta.env.WORDPRESS_URL}/wp-json/wp/v2/pages?slug=${slug}`
+export async function getPageBySlug(slug: string): Promise<WpPage | null> {
+    let res: WpPage | null = null;
+    let pageArray: WpPage[] | null = null;
+    const fetchPage = await fetch(`${wordpress}/wp-json/wp/v2/pages/`);
+    if (fetchPage.ok) {
+        pageArray = await fetchPage.json();
+        if (pageArray && pageArray.length > 0) {
+            res = pageArray.filter((val) => val.link === `${wordpress}/${slug}/`)[0] ?? null;
+        }
+    }
+    return res;
 }
